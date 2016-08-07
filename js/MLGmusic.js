0b9b3dfa2ce4cd135d6cf75876d807fb6161500e
zllmycodes <zllmyjobs@163.com>
2016-08-07 22:00:59 +0800

    init MLGmusic files

---
+++ js/MLGmusic.js
@@ -0,0 +1,292 @@
+var lrcsArr = ["[00:02.00]夜蝶\n[00:04.00]\n[00:06.00]演唱：SNH48\n[00:08.00]\n[00:17.25]悄悄的 今晚相遇这里\n"+
+"[00:20.87]不要向任何人提起\n[00:23.48]要保密\n[00:24.17]\n[00:24.72]悄悄的 来到隐秘之地\n"+
+"[00:28.35]接下来 做什么事情\n[00:31.43]\n[00:32.35]Ah 月光在辉映 这般妖娆\n"+
+"[00:36.12]Ah 云雾变得浓密 邀请了你\n[00:40.01]来到了这里\n[00:41.61]你有什么目的\n"+
+"[00:43.46]在这迷人少女花园里\n[00:48.41]黑暗的夜 原来蕴藏这般甜蜜\n[00:52.91]不知 不觉\n"+
+"[00:53.87]这危险却已成禁忌\n[00:56.73]一步一步 心跳不息\n[00:58.80]是否愿意 当然愿意\n"+
+"[01:00.78]充满危险的游戏\n[01:03.86]\n[01:04.43]无意间两双对质的眼睛\n[01:07.48]\n"+
+"[01:08.01]为何 为何\n[01:08.91]沉默让彼此更坚定\n[01:11.32]\n[01:11.94]伸出的手 向你靠近\n"+
+"[01:13.96]交错的情 你我的心\n[01:15.76]雌雄相辉映 夜空中蝶影\n[01:21.69]\n[01:32.90]秘密的 变得不像我自己\n"+
+"[01:36.41]妈妈如果知道一定 很生气\n[01:39.82]\n"+"[01:40.56]秘密的 就这样不搭理\n"+
+"[01:44.04]就可以 纵情直到天明\n[01:47.23]\n[01:47.93]Ah 这些不曾遇见\n[01:51.09]未见花名\n"+
+"[01:51.97]Ah 又酸甜又浓郁\n[01:54.57]香四溢\n[01:55.53]别让我心急\n[01:57.25]我会带领着你\n"+
+"[01:59.11]来到迷人少女花园里\n[02:03.57]\n[02:04.11]爱情总是让人意乱捉摸不定\n[02:08.54]不知 不觉\n"+
+"[02:09.41]这双唇逐渐的靠近\n[02:12.27]这不可以 有何关系\n[02:14.45]不要行进 可爱的你\n"+
+"[02:16.25]跨过临界线的距离\n[02:19.47]\n[02:19.98]爱情总是 深陷这迷情\n[02:23.56]完全 已经\n"+
+"[02:24.46]无法再压抑的呼吸\n[02:26.83]\n[02:27.53]灼热的情 本能反应\n[02:29.58]恐惧的心 释放双翼\n"+
+"[02:31.38]雌雄相辉映\n[02:33.79]夜空中蝶影\n[02:38.63]\n[03:04.67]黑暗的夜 原来蕴藏这般甜蜜\n"+
+"[03:09.00]不知 不觉\n[03:09.84]这危险却已成禁忌\n[03:12.27]\n[03:12.79]一步一步 心跳不息\n"+
+"[03:14.88]是否愿意 当然愿意\n[03:16.74]充满危险的游戏\n[03:20.47]无意间两双对质的眼睛\n"+
+"[03:24.02]为何 为何\n[03:24.94]沉默让彼此更坚定\n[03:27.35]\n[03:28.04]伸出的手 向你靠近\n"+
+"[03:30.02]交错的情 你我的心\n[03:31.80]雌雄相辉映\n[03:34.24]夜空中蝶影\n[03:38.64]",
+"[00:00.60]斯德哥尔摩情人\n[00:02.56]词：林夕 曲：C. Y. Kong\n[00:04.42]演唱：陈奕迅\n[00:06.46]\n"+
+"[00:13.65]逃避 分开的孤独\n[00:16.83]情愿 一起不舒服\n[00:20.20]其实你那占有欲 咬噬我血肉\n"+
+"[00:23.93]怕我也有份 教育\n[00:26.69]未能做 空虚的枯木\n[00:29.94]滞留在 挤涌的监狱\n"+
+"[00:33.49]明白你有控制欲 我为了大局\n[00:37.20]上了瘾也不 戒 毒\n[00:40.00]没有献出我的脸怎 拍响\n"+
+"[00:43.29]没有两巴掌 怎制止 痕痒\n[00:46.69]糊涂地 软弱当善良\n[00:48.57]谁就 这样变善良\n"+
+"[00:50.52]你更放肆得漂 亮\n[00:53.51]也许 当我感到窒息 想 逃亡\n[00:57.19]却未戒掉浴血 的 欲望\n"+
+"[01:00.43]也许早已恋上共绑匪苦 海慈航\n[01:03.63]原谅你越爱越恶\n[01:05.45]满足我 预计的失 望\n"+
+"[01:07.69]是盲目地伟大成狂\n[01:10.12]还是受害 受用 犯贱 犯到 被虐成狂\n[01:13.56]能为你忍 受\n"+
+"[01:15.20]然后当享受 那 又 何 妨\n[01:30.18]为逃避 轻松得孤独\n[01:33.39]便宁愿 紧张得舒服\n"+
+"[01:36.75]无谓设计了布局 这样快结局\n[01:40.60]爱与痛也不到肉\n[01:43.36]像战争片 最好有死有伤\n"+
+"[01:46.71]未吓到 尖叫 哭也不流畅\n[01:49.96]完全为 配合我软弱 才令 你乐意肆虐\n[01:53.88]作恶也要好对象\n"+
+"[01:56.83]也许 早已不觉窒息 想投降\n[02:00.52]舔尽 你赠我的 一额汗\n[02:03.69]也许早已适应 就此跟绑 匪 同床\n"+
+"[02:06.83]谁料你 谁料我 能合作到 爱死对方\n[02:09.99]应该也 不只一次 幻想怎么 逃亡\n[02:13.90]却未戒掉 妥协 的 欲望\n"+
+"[02:17.06]也许早已恋上共绑匪 苦 海慈航\n[02:20.09]情欲 要被你 勒索\n[02:22.02]也许有助 刺激心脏\n"+
+"[02:24.09]是盲目地伟大成狂\n[02:27.06]还是受害 受用 犯贱 犯到 被虐成狂\n[02:30.33]能为你忍 受\n"+
+"[02:31.80]然后当享受 那 又 何 妨\n[02:46.91]没有我 给你操纵的 快感\n[02:49.97]问你的 兴奋 知觉 怎膨胀\n"+
+"[02:53.22]完全为 配合我软弱\n[02:55.19]才令你 乐意肆虐\n[02:57.20]作恶也要好 对 象\n[03:01.82]也许 早已不觉窒息 想投降\n"+
+"[03:05.50]舔尽 你赠我的 一额汗\n[03:08.70]也许早已适应 就此跟绑匪 同床\n[03:11.82]谁料你 谁料我 能合作到 爱死对方\n"+
+"[03:14.89]应该也 不只一次 幻想怎么逃亡\n[03:18.82]却未戒掉 妥协 的 欲望\n[03:21.94]也许早已恋上共绑匪 苦海慈航\n"+
+"[03:25.16]情欲 要被你 勒索\n[03:27.15]也许有助 刺激心脏\n[03:29.38]但无论是 伟大成狂\n[03:31.76]还是 受害 受用 犯贱 犯到 被虐成狂\n"+
+"[03:35.17]看着 是谁令幸福 给殓葬\n[03:38.53]别喊冤 别叫屈 别诉苦 在这 宗 惨 案\n[03:42.10]全赖我忍受 才令你享受\n"+
+"[03:48.45]我是同谋\n[03:55.37]绝对是同谋\n[03:56.37]"];
+
+//Music引用类型
+function Music(music,lyric){
+    this.music = music;
+    this.lyric = lyric;
+    //用于计数当前歌词
+    this.lrcNum = 0;
+}
+Music.prototype = {
+    constructor:Music,
+    //解析歌词
+    parseLyric:function(){
+        if(this.lyric){
+            var lrcArr = this.lyric.split("\n");
+            var lrcObjArr = [];
+            for(var i=0;i<lrcArr.length;i++){
+                var lrcObj = lrcArr[i];
+                var timeExg = /\[\d*\:\d*((\:|\.)\d*)*\]/g;
+                var timeArr = lrcObj.match(timeExg);
+                if(!timeArr)continue;
+                var lrcContent = lrcObj.replace(timeExg,'');
+
+                for(var j=0;j<timeArr.length;j++){
+                    var min = Number(String(timeArr[j].match(/\[\d*/i)).slice(1));
+                    var sec = Number(String(timeArr[j].match(/\:\d*/i)).slice(1));
+                    var myTime = min*60+sec;
+                    lrcObjArr.push({"myTime":myTime,"lrcContent":lrcContent});
+                }   
+            }
+            return lrcObjArr;
+        }else{
+            return null;
+        }
+    },
+    addLrc:function(ulObj){
+        ulObj.html(" ");
+        var lrcObjArr = this.parseLyric();
+        if(!lrcObjArr){
+            ulObj.html("<h4>没有歌词</h4>");
+        }else{
+            for(var i=0;i<lrcObjArr.length;i++){
+                var liObj = $("<li/>");
+                liObj.html(lrcObjArr[i].lrcContent);
+                ulObj.append(liObj);
+            }
+        }
+    },
+    scrollLrc:function(){
+        var curTime = parseInt($("#audioPlay")[0].currentTime);
+        var lrcShow = $("#lrcShow");
+        var lrcObjArr = this.parseLyric();
+        if(!lrcShow.find("h4")[0]){
+            var num = this.lrcNum;
+            var keepTime = 100;  //歌词整齐了，之前500太长
+            if(num >= lrcObjArr.length){
+                this.lrcNum = 0;
+                return;
+            }
+            if(curTime >= lrcObjArr[num].myTime){
+                lrcShow.find("li").get(num).setAttribute("class","lrcActive");
+                if(num != 0){
+                    lrcShow.find("li").get(num-1).setAttribute("class","");
+                }
+                if(num>8 && lrcObjArr[num].lrcContent){
+                    lrcShow.css("top",(parseInt(lrcShow.css("top"))-25+"px"));
+                }else if(num<=8){
+                    lrcShow.css("top","0px");
+                }
+                this.lrcNum++;
+                keepTime = 1000;
+            }
+            var _this = this;
+            playClock = setTimeout(function(){
+                _this.scrollLrc();
+            },keepTime);
+        }
+    },
+    //点击音乐
+    clickMusic:function(liObj){
+        liObj.innerHTML = this.music;
+        var _this = this;
+        liObj.onclick = function(){
+            _this.musicPlay(liObj);
+        };
+    },
+    changeMusic:function(liObj){
+        //设置播放列表样式
+        this.musicPlay(liObj);
+    },
+    musicPlay:function(liObj){
+        //将歌词计数重新置零
+        this.lrcNum = 0;
+        //如果有之前设置的歌词滚动，清除
+        if(playClock){
+            clearTimeout(playClock);
+        }
+        var liArr = $("#musicList li");
+        for(var k=0;k<liArr.length;k++){
+            liArr[k].setAttribute("class"," ");
+        }
+        liObj.setAttribute("class","musicListActive");
+        //改变播放按钮|暂停按钮
+        var playClass = $("#playMusic").attr("class").replace("play","pause");
+        $("#playMusic").attr("class",playClass);
+        //音乐播放
+        var audioObj = document.getElementById("audioPlay");
+        audioObj.src = this.music+".mp3";
+        audioObj.play();
+        //进度条和时间
+        setTimeout(progressShow,100);
+        //歌词显示
+        this.addLrc($("#lrcShow"));
+        //歌曲名字部分        
+        $("#lyricTitle").html(liObj.innerHTML);
+    }
+};
+
+//数据部分
+var musicObjs = [];
+musicObjs[0] = new Music("SNH48 - 夜蝶",lrcsArr[0]);
+musicObjs[1] = new Music("陈奕迅 - 斯德哥尔摩情人",lrcsArr[1]);
+
+$(document).ready(function($){
+    //展示音乐列表
+    $("#musicList").html("");
+    var num = [];
+    for(var i=0;i<musicObjs.length;i++){
+        var liObj = document.createElement("li");
+        num[i] = function(n,liObj){
+            return function(){
+                musicObjs[n].clickMusic(liObj);
+            }
+        }(i,liObj);
+        $("#musicList").append(liObj);
+    } 
+    for(var j=0;j<num.length;j++){
+        num[j]();
+        num[j] = null;
+    }
+});
+
+var playClock;
+//播放中的音乐，有歌词的也在滚动
+$("#audioPlay").bind("play",function(){
+    var activeLiIndx = getActiveMusic();
+    musicObjs[activeLiIndx].scrollLrc();
+});
+//循环播放，没有事件监听，自己写
+//把循环设置删除，自己监听结束，再重新播放
+$("#audioPlay").bind("ended",function(){
+    var reptMusic = document.getElementById("reptMusic");
+    clearTimeout(playClock);
+    if(reptMusic.getAttribute("class").indexOf("repeat")>=0){
+        var activeLiIndx = getActiveMusic();
+        musicObjs[activeLiIndx].addLrc($("#lrcShow"));
+        this.play();
+    }
+});
+
+//切换曲目控件
+//上一首
+$("#prevMusic").click(function(){
+    var liObj = $("#musicList li");
+    var activeLiIndx = getActiveMusic();
+    if(activeLiIndx<liObj.length){
+        if(activeLiIndx > 0){
+            musicObjs[activeLiIndx-1].changeMusic(liObj[activeLiIndx-1]);
+        }
+    }
+});
+//下一首
+$("#nextMusic").click(function(){
+    var liObj = $("#musicList li");
+    var activeLiIndx = getActiveMusic();
+    if(getActiveMusic()>=0){
+        if(activeLiIndx < liObj.length-1){
+            musicObjs[activeLiIndx+1].changeMusic(liObj[activeLiIndx+1]);
+        }
+    }
+});
+//获取当前播放的音乐
+function getActiveMusic(){
+    var musicLi = $("#musicList li");
+    for(var i=0;i<musicLi.length;i++){
+        if(musicLi[i].getAttribute("class").indexOf("musicListActive")>=0){
+            break;
+        }
+    }
+    return i;
+}
+
+//进度条和时间的显示
+var timeClock;
+function progressShow(){
+    //时间的显示
+    var audioObj = $("#audioPlay")[0];
+    var durationMin = parseInt(audioObj.duration/60);
+    var durationSec = parseInt(audioObj.duration-durationMin*60);
+    var curMin = parseInt(audioObj.currentTime/60);
+    var curSec = parseInt(audioObj.currentTime-curMin*60);
+    if(curSec<10){
+        curSec = "0"+curSec;
+    }
+    $("#musicTime").html(curMin+":"+curSec+"/"+(durationMin+":"+durationSec));
+    //进度条的显示
+    $("#progressBar").css("width",((audioObj.currentTime/audioObj.duration)*100+"%"));
+
+    //重复调用
+    timeClock = setTimeout(arguments.callee,1000);
+}
+//控件操作
+//播放机|暂停键的操作
+$("#playMusic").click(function(){
+    var classVal = $(this).attr("class");
+    if(classVal.indexOf("play")>=0){
+        classVal = classVal.replace("play","pause");
+        $(this).attr("class",classVal);
+        $("#audioPlay")[0].play();
+    }else{
+        classVal = classVal.replace("pause","play");
+        $(this).attr("class",classVal);
+        $("#audioPlay")[0].pause();
+    }
+});
+//静音键
+$("#mute").click(function(){
+    var classVal = $(this).attr("class");
+    if(classVal.indexOf("up")>=0){
+        classVal = classVal.replace("up","off");
+        $(this).attr("class",classVal);
+        $("#audioPlay")[0].muted = true;
+    }else{
+        classVal = classVal.replace("off","up");
+        $(this).attr("class",classVal);
+        $("#audioPlay")[0].muted = false;
+    }
+});
+//循环键的操作
+$("#reptMusic").click(function(){
+    var classVal = $(this).attr("class");
+    if(classVal.indexOf("repeat")>0){
+        classVal = classVal.replace("repeat","arrow-right");
+        $(this).attr("class",classVal);
+    }else{
+        classVal = classVal.replace("arrow-right","repeat");
+        $(this).attr("class",classVal);
+    }
+});
+
